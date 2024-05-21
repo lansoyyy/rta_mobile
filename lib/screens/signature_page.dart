af8d86da77ee0bb8f5bf74edceff01793c2e9c61
Lans12345 <olanalans12345@gmail.com>
2024-05-21 16:36:00 +0800

    added write func

--- lib/screens/signature_page.dart
+++ lib/screens/signature_page.dart
@@ -1,5 +1,16 @@
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_signature_pad/flutter_signature_pad.dart';
+import 'package:rta_mobile/screens/next_screen.dart';
+import 'package:rta_mobile/services/add_ticket.dart';
+import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
+import 'package:path/path.dart' as path;
+import 'package:image_picker/image_picker.dart';
+import 'package:flutter/foundation.dart';
+import 'dart:io';
+
+import 'package:rta_mobile/widgets/toast_widget.dart';
+import 'package:screenshot/screenshot.dart';
 
 class SignaturePage extends StatefulWidget {
   Map data;
@@ -12,6 +23,13 @@ class SignaturePage extends StatefulWidget {
 
 class _SignaturePageState extends State<SignaturePage> {
   final GlobalKey<SignatureState> _signatureKey = GlobalKey();
+
+  late File imageFile;
+
+  late String imageURL = '';
+
+  bool hasUploaded = false;
+  ScreenshotController screenshotController = ScreenshotController();
 
   @override
   Widget build(BuildContext context) {
@@ -38,41 +56,40 @@ class _SignaturePageState extends State<SignaturePage> {
             fit: BoxFit.cover,
           ),
         ),
-        height: double.infinity,
-        width: double.infinity,
         child: Padding(
           padding: const EdgeInsets.fromLTRB(10, 20, 10, 20),
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                Container(
-                  height: 770,
-                  width: 500,
-                  decoration: BoxDecoration(
-                    color: const Color.fromARGB(255, 236, 234, 234),
-                    border: Border.all(
-                      color: const Color.fromARGB(255, 245, 4, 4),
-                      width: 1,
-                    ),
-                    borderRadius: BorderRadius.circular(1),
+          child: Column(
+            children: [
+              Container(
+                height: 500,
+                width: 500,
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 236, 234, 234),
+                  border: Border.all(
+                    color: const Color.fromARGB(255, 245, 4, 4),
+                    width: 1,
                   ),
-                  child: Column(
-                    children: [
-                      const SizedBox(height: 10),
-                      Expanded(
-                        child: Column(
-                          children: [
-                            const Text(
-                              "Violator's Signature",
-                              style: TextStyle(
-                                color: Colors.red,
-                                fontSize: 18,
-                                fontWeight: FontWeight.bold,
-                              ),
+                  borderRadius: BorderRadius.circular(1),
+                ),
+                child: Column(
+                  children: [
+                    const SizedBox(height: 10),
+                    Expanded(
+                      child: Column(
+                        children: [
+                          const Text(
+                            "Violator's Signature",
+                            style: TextStyle(
+                              color: Colors.red,
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
                             ),
-                            const SizedBox(height: 10),
-                            SizedBox(
-                              height: 550,
+                          ),
+                          const SizedBox(height: 10),
+                          Screenshot(
+                            controller: screenshotController,
+                            child: SizedBox(
+                              height: 300,
                               width: 350,
                               child: Container(
                                 padding: const EdgeInsets.all(16.0),
@@ -84,42 +101,114 @@ class _SignaturePageState extends State<SignaturePage> {
                                 ),
                               ),
                             ),
-                          ],
-                        ),
-                      ),
-                      const SizedBox(height: 20),
-                      ElevatedButton(
-                        onPressed: () {
-                          _signatureKey.currentState?.clear();
-                        },
-                        style: ElevatedButton.styleFrom(
-                          minimumSize: const Size(350, 40),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(20),
                           ),
-                        ),
-                        child: const Text('CLEAR SIGNATURE'),
+                        ],
                       ),
-                      MaterialButton(
-                        onPressed: () {},
-                        minWidth: 350,
-                        height: 40,
-                        color: const Color.fromARGB(255, 55, 161, 248),
-                        textColor: Colors.white,
+                    ),
+                    const SizedBox(height: 20),
+                    ElevatedButton(
+                      onPressed: () {
+                        _signatureKey.currentState?.clear();
+                      },
+                      style: ElevatedButton.styleFrom(
+                        minimumSize: const Size(350, 40),
                         shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(20),
                         ),
-                        child: const Text(
-                          'ENTER',
-                          style: TextStyle(color: Colors.white),
-                        ),
                       ),
-                      const SizedBox(height: 20),
-                    ],
-                  ),
+                      child: const Text('CLEAR SIGNATURE'),
+                    ),
+                    hasUploaded
+                        ? MaterialButton(
+                            onPressed: () {
+                              addTicket(
+                                widget.data['violations'],
+                                widget.data['name'],
+                                widget.data['address'],
+                                widget.data['licenseno'],
+                                widget.data['expiry'],
+                                widget.data['gender'],
+                                widget.data['licensetype'],
+                                widget.data['bday'],
+                                widget.data['nationality'],
+                                widget.data['height'],
+                                widget.data['weight'],
+                                widget.data['plateno'],
+                                widget.data['owner'],
+                                widget.data['owneraddress'],
+                                widget.data['maker'],
+                                widget.data['color'],
+                                widget.data['model'],
+                                widget.data['number'],
+                                imageURL,
+                              );
+                              Navigator.of(context).pushReplacement(
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                          const NextScreen()));
+                              showToast('Ticket recorded succesfully!');
+                            },
+                            minWidth: 350,
+                            height: 40,
+                            color: const Color.fromARGB(255, 55, 161, 248),
+                            textColor: Colors.white,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            child: const Text(
+                              'ENTER',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          )
+                        : MaterialButton(
+                            onPressed: () async {
+                              try {
+                                // Capture the screenshot as a Uint8List
+                                Uint8List? bytes =
+                                    await screenshotController.capture();
+
+                                if (bytes != null) {
+                                  // Create a unique file name
+
+                                  // Create a reference to the Firebase Storage location
+                                  Reference storageReference =
+                                      FirebaseStorage.instance.ref().child(
+                                          'signatures/${DateTime.now()}.png');
+
+                                  // Upload the screenshot
+                                  UploadTask uploadTask =
+                                      storageReference.putData(
+                                          bytes,
+                                          SettableMetadata(
+                                              contentType: 'image/png'));
+
+                                  // Get the download URL after the upload completes
+                                  TaskSnapshot taskSnapshot = await uploadTask;
+
+                                  setState(() async {
+                                    imageURL =
+                                        await taskSnapshot.ref.getDownloadURL();
+                                    hasUploaded = true;
+                                  });
+                                } else {}
+                              } catch (e) {
+                                print(e);
+                              }
+                            },
+                            minWidth: 350,
+                            height: 40,
+                            color: const Color.fromARGB(255, 55, 161, 248),
+                            textColor: Colors.white,
+                            shape: const RoundedRectangleBorder(),
+                            child: const Text(
+                              'CONFIRM',
+                            ),
+                          ),
+                    const SizedBox(height: 20),
+                  ],
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
---
+++ lib/services/add_ticket.dart
@@ -0,0 +1,57 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+Future<String> addTicket(
+  List violations,
+  name,
+  address,
+  licenseno,
+  expiry,
+  gender,
+  licensetype,
+  bday,
+  nationality,
+  height,
+  weight,
+  plateno,
+  owner,
+  owneraddress,
+  maker,
+  color,
+  model,
+  number,
+  signature,
+) async {
+  final docUser = FirebaseFirestore.instance.collection('Tickets').doc();
+
+  final json = {
+    'violations': violations,
+    'name': name,
+    'address': address,
+    'licenseno': licenseno,
+    'expiry': expiry,
+    'gender': gender,
+    'bday': bday,
+    'nationality': nationality,
+    'height': height,
+    'weight': weight,
+    'plateno': plateno,
+    'owner': owner,
+    'owneraddress': owneraddress,
+    'maker': maker,
+    'color': color,
+    'model': model,
+    'number': number,
+    'licensetype': licensetype,
+    'uid': FirebaseAuth.instance.currentUser!.uid,
+    'id': docUser.id,
+    'day': DateTime.now().day,
+    'month': DateTime.now().month,
+    'year': DateTime.now().year,
+    'dateTime': DateTime.now(),
+    'signature': signature,
+  };
+
+  await docUser.set(json);
+  return docUser.id;
+}
